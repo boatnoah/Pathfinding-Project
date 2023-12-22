58c91a838be44fa54cb65e1456b897b672506c50
boatnoah <noahskim7@gmail.com>
2023-12-21 22:55:51 -0800

    restructured code

--- box.py
+++ box.py
@@ -9,9 +9,33 @@ class Box:
         self.isWall = False
         self.isTarget = False
         self.isStart = False
-    
+        self.isQueued = False
+        self.isVisited = False
+        self.prior = None
+        self.distance = float("inf")
+        self.neighbors = []
+        
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+
     def draw_box(self, canvas, color):
         pygame.draw.rect(canvas, color, (self.x * BOX_WIDTH, self.y * BOX_HEIGHT, BOX_WIDTH - 2, BOX_HEIGHT - 2))
 
+    def _set_neighbors(self):
+        if self.x > 0:
+            self.neighbors.append((GRID[self.x - 1][self.y], 1 if not GRID[self.x - 1][self.y].isWall else 5))
+        if self.x < COLUMNS - 1:
+            self.neighbors.append((GRID[self.x + 1][self.y], 1 if not GRID[self.x + 1][self.y].isWall else 5))
+        if self.y > 0:
+            self.neighbors.append((GRID[self.x][self.y - 1], 1 if not GRID[self.x][self.y - 1].isWall else 5))
+        if self.y < ROWS - 1:
+            self.neighbors.append((GRID[self.x][self.y + 1], 1 if not GRID[self.x][self.y + 1].isWall else 5))
+    
+   
+
+    
+
+        
 
 
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import sys
 from settings import *
 from box import Box
 from button import Button
+from queue import PriorityQueue
+import random
 
-
-
-#Initialize grid
-GRID = []
 
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+priority_queue = PriorityQueue()#Dijkstra
+queue = [] #BFS
+heap = [] #A-star
+stack = [] #DFS
+path = []
 
 def get_font(size): 
     return pygame.font.Font("assets/font.ttf", size)
@@ -22,35 +26,45 @@ def initialize_grid(rows, col):
         tmp = []
         for j in range(col):
             tmp.append(Box(i, j))
-        GRID.append(tmp) 
+        GRID.append(tmp)
+    
+    for i in range(ROWS):
+        for j in range(COLUMNS):
+            GRID[i][j]._set_neighbors()
+    
 
-def create_grid(rows, col):
-    for i in range(col):
-        for j in range(rows):
-            cell = GRID[i][j]
-            cell.draw_box(screen, (50, 50, 50))
-            if cell.isWall:
-                cell.draw_box(screen, "black")
-            if cell.isTarget:
-                cell.draw_box(screen, "red")
-            if cell.isStart:
-                cell.draw_box(screen, "blue")
 
-def algorithm_menu_screen():
+
+
+
+def algorithm_menu_screen(first_run=True):
     running = True
     button_height, button_spacing = 100, 50
     while running:
+        screen.fill((50, 50, 50))
         MENU_MOUSE_POS = pygame.mouse.get_pos()
         MENU_TEXT = get_font(35).render("Choose an Algorithm", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(WINDOW_WIDTH // 2, 50))
-        DIJKSTRA_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), pos=(WINDOW_WIDTH // 2, 150), 
-                                text_input="DIJKSTRA", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        ASTAR_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), pos=(WINDOW_WIDTH // 2, 150 + button_height + button_spacing), 
-                            text_input="A* Search", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        BFS_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), pos=(WINDOW_WIDTH // 2, 150 + 2 * (button_height + button_spacing)), 
-                            text_input="BFS", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        DFS_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), pos=(WINDOW_WIDTH // 2, 150 + 3 * (button_height + button_spacing)), 
-                            text_input="DFS", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+
+        DIJKSTRA_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), 
+                                 pos=(WINDOW_WIDTH // 2, 150), 
+                                text_input="DIJKSTRA", font=get_font(40), 
+                                base_color="#d7fcd4", hovering_color="White")
+        
+        ASTAR_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), 
+                              pos=(WINDOW_WIDTH // 2, 150 + button_height + button_spacing),
+                              text_input="A* Search", font=get_font(40), 
+                              base_color="#d7fcd4", hovering_color="White")
+        
+        BFS_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), 
+                            pos=(WINDOW_WIDTH // 2, 150 + 2 * (button_height + button_spacing)), 
+                            text_input="BFS", font=get_font(40), 
+                            base_color="#d7fcd4", hovering_color="White")
+        
+        DFS_BUTTON = Button(image=pygame.image.load("assets/buttonBG.png"), 
+                            pos=(WINDOW_WIDTH // 2, 150 + 3 * (button_height + button_spacing)), 
+                            text_input="DFS", font=get_font(40), 
+                            base_color="#d7fcd4", hovering_color="White")
         
         screen.blit(MENU_TEXT, MENU_RECT)
 
@@ -62,26 +76,30 @@ def algorithm_menu_screen():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: 
                 if DIJKSTRA_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    main("Dijkstra")
+                    main("Dijkstra", first_run)
                 if ASTAR_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    main("A-star")
+                    main("A-star", first_run)
                 if BFS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    main("BFS")
+                    main("BFS", first_run)
                 if DFS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    main("DFS")
+                    main("DFS", first_run)
 
         
         pygame.display.update()
 
-def main(typeOfAlgo): #grid screen
+def main(typeOfAlgo, first_run): #grid screen
+    global priority_queue, queue, heap, stack, path, is_starting_set, is_target_set, start_cell, target_cell, algorithm_to_run, GRID
     algorithm_to_run = typeOfAlgo 
     print(algorithm_to_run)
-    is_starting_set = False
-    is_target_set = False
+    if first_run:
+        is_starting_set = False
+        is_target_set = False
+    
     running = True
-    initialize_grid(ROWS, COLUMNS) 
+    begin_search = False
+    searching = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -98,47 +116,144 @@ def main(typeOfAlgo): #grid screen
                 x, y = pygame.mouse.get_pos()
                 i = x // BOX_WIDTH
                 j = y // BOX_HEIGHT
-                if event.button == 1 and not is_starting_set:
-                    start_cell = GRID[i][j]
-                    start_cell.isStart = True
-                    is_starting_set = True
-                elif event.button == 1 and not is_target_set:
-                    target_cell = GRID[i][j]
-                    target_cell.isTarget = True
-                    is_target_set = True
+                if event.button == 1:
+                    if not is_starting_set:
+                        start_cell = GRID[i][j]
+                        start_cell.isStart = True
+                        is_starting_set = True
+                        if algorithm_to_run == "Dijkstra":
+                            start_cell.isQueued = True
+                        elif algorithm_to_run == "BFS":
+                            start_cell.isQueued = True
+                    elif not is_target_set:
+                        target_cell = GRID[i][j]
+                        target_cell.isTarget = True
+                        is_target_set = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_c: #clear the current state of the grid
                     GRID.clear()
                     initialize_grid(ROWS, COLUMNS)
-                    create_grid(ROWS, COLUMNS)
                     is_starting_set = False
-                    is_target_set = False
-                    typeOfAlgo = "" 
-                
-                if event.key == pygame.K_ESCAPE:
-                    GRID.clear()
-                    initialize_grid(ROWS, COLUMNS)
-                    create_grid(ROWS, COLUMNS)
-                    is_starting_set = False
-                    is_target_set = False
-                    algorithm_menu_screen()
+                    is_target_set = False 
+                    begin_search = False
+                    searching = True
+                    path.clear()
+                    priority_queue = PriorityQueue()
+                    queue.clear()
+                    heap.clear()
+                if event.key == pygame.K_ESCAPE: 
+                    #still preserve the starting and target positions and walls
+                    for i in range(ROWS):
+                        for j in range(COLUMNS):
+                            cell = GRID[i][j]
+                            if cell.isVisited:
+                                cell.isVisited = False
+                            if cell.isQueued:
+                                cell.isQueued = False
+                            if cell.prior is not None:
+                                cell.prior = None
+                            if cell.distance != float("inf"):
+                                cell.distance = float("inf")
+                            
                     
-                if event.key == pygame.K_RETURN and is_starting_set and is_target_set:
-                    if algorithm_to_run == "Dijkstra":
-                        pass
-                    elif algorithm_to_run == "A-star":
-                        pass
-                    elif algorithm_to_run == "BFS":
-                        pass
-                    elif algorithm_to_run == "DFS":
-                        pass
+                    path.clear()
+                    priority_queue = PriorityQueue()
+                    queue.clear()
+                    heap.clear()
+                    stack.clear()
+                    is_starting_set = True
+                    is_target_set = True
+                    
+                    
+                    algorithm_menu_screen(False)
 
+                    
+                if event.key == pygame.K_RETURN and is_starting_set and is_target_set: #to ensure algorithm will run with a start and end
+                    begin_search = True
+
+        if begin_search:
+            if algorithm_to_run == "Dijkstra":
+                print("Running Dijkstra")
+                if priority_queue.empty() and searching:
+                    priority_queue.put((0, start_cell))
+                    start_cell.distance = 0
+            
+
+                if not priority_queue.empty() and searching:
+                    # Step 2: Process nodes in the priority queue
+                    current_distance, current_box = priority_queue.get()
+
+                    if not current_box.isVisited:  # Skip if already visited
+                
+                        current_box.isVisited = True  # Mark as visited
+
+                        if current_box.isTarget:
+                            searching = False
+                            while current_box.prior is not None:
+                                path.append(current_box.prior)
+                                current_box = current_box.prior
+                                pygame.event.pump()  # Process events during delay
+
+            
+                        for neighbor, edge_weight in current_box.neighbors:
+                            if not neighbor.isVisited and not neighbor.isWall:
+                                tentative_distance = current_distance + edge_weight
+
+                                if tentative_distance < neighbor.distance:
+                                    neighbor.distance = tentative_distance
+                                    neighbor.prior = current_box
+                                    priority_queue.put((tentative_distance, neighbor))
+                                    neighbor.isQueued = True  # Mark as queued
+
+
+            elif algorithm_to_run == "A-star":
+                pass
+            elif algorithm_to_run == "BFS":
+                print("Running BFS")
+                if not queue and searching:
+                    queue.append(start_cell)
+        
+                if queue and searching:
+                    print("made it here")
+                    current_box = queue.pop(0)
+                    current_box.isVisited = True
+                    if current_box.isTarget:
+                        searching = False
+                        while current_box.prior != start_cell:
+                            path.append(current_box.prior)
+                            current_box = current_box.prior
+                            pygame.event.pump()  # Process events during delay
+                    else:
+                        for neighbor, _ in current_box.neighbors:
+                            if not neighbor.isQueued and not neighbor.isWall:
+                                neighbor.isQueued = True
+                                neighbor.prior = current_box
+                                queue.append(neighbor)
+        
+            elif algorithm_to_run == "DFS":
+                pass
+            
         screen.fill((0, 0, 0))
-
-        create_grid(ROWS, COLUMNS)
+        for i in range(ROWS):
+            for j in range(COLUMNS):
+                cell = GRID[i][j]
+                cell.draw_box(screen, (50, 50, 50))
+                if cell.isQueued:
+                    cell.draw_box(screen, "yellow")
+                if cell.isVisited:
+                    cell.draw_box(screen, "green")
+                if cell in path:
+                    cell.draw_box(screen, "cyan")
+                if cell.isWall:
+                    cell.draw_box(screen, "black")
+                if cell.isTarget:
+                    cell.draw_box(screen, "red")
+                if cell.isStart:
+                    cell.draw_box(screen, "blue")
+                
         pygame.display.flip()
         
 
-
+initialize_grid(ROWS, COLUMNS)
 algorithm_menu_screen()
 
